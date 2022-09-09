41b5d7f256025e90cd83a61a5f32f779360e028d
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-09-10 02:31:36 +0900

    feat: Wrote some codes to import library

--- Logic/src/com/logic/Logic.java
+++ Logic/src/com/logic/Logic.java
@@ -1,10 +1,16 @@
 package com.logic;
 
+import com.game.library.Screen;
+
 public class Logic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		LogicScreen logicScreen = new LogicScreen(30, 30);
+		
+		logicScreen.init();
+		logicScreen.draw();
+		
 	}
 
 }
