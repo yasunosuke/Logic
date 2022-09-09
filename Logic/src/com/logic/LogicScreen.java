cfef86ac7c1ba7dfa7f3050557e9ac1b62b2180c
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-09-09 18:09:50 +0900

    feat; New class

---
+++ Logic/src/com/logic/LogicScreen.java
@@ -0,0 +1,36 @@
+package com.logic;
+
+import com.game.library.Screen;
+
+public class LogicScreen extends Screen {
+	
+	private String[][] defaultField = {
+			{}
+	};
+	
+	private String[][] screen = new String[SCREEN_HEIGHT][SCREEN_WIDTH];
+	
+	public LogicScreen(int SCREEN_HEIGHT, int SCREEN_WIDTH) {
+		super(SCREEN_HEIGHT, SCREEN_WIDTH);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void init() {
+		
+	}
+	
+	private int makeQuestion() {
+		for(int )
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+	
+	
+
+	
